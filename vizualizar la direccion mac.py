603503d0c919cbef39fbaeeee0c04f6fb8d9f4f4
galho7 <66712935+galho7@users.noreply.github.com>
2024-10-16 14:36:17 -0500

    Update vizualizar la direccion mac.py

--- vizualizar la direccion mac.py
+++ vizualizar la direccion mac.py
@@ -2,4 +2,12 @@ import network, ubinascii
 ap_if = network.WLAN(network.AP_IF)
 ap_if.active(True)
 print("ESSID:", ap_if.config("essid"))
-print("Direccion MAC: ",ubinascii.hexlify(network.WLAN().config("mac"),":").decode()) 
+print("Direccion MAC: ",ubinascii.hexlify(network.WLAN().config("mac"),":").decode())
+# Dirección MAC en formato de texto
+mac_str = "b0:a7:32:d7:5e:80"
+
+# Convertir la dirección MAC en una lista de bytes
+mac_bytes = bytes(int(b, 16) for b in mac_str.split(':'))
+
+# Imprimir la dirección MAC en formato de bytes
+print(mac_bytes)
